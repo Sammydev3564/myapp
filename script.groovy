6afb6d1081673b8cdb9e14cec040be0b9e70865e
Sammydev3564 <dadasammyajibola@gmail.com>
2023-01-07 14:14:49 +0100

    updated docker credential

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerlogin', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t sammydev3564/my-repo:v2.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push sammydev3564/my-repo:v2.1 '
