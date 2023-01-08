eea2f7a24099a6c504e66a1b37d274d277c46e7f
Sammydev3564 <101530687+Sammydev3564@users.noreply.github.com>
2023-01-08 18:33:29 +0100

    updated jenkinsfile

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerlogin', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t sammydev3564/my-repo:v2.0 .'
+        sh 'docker build -t sammydev3564/my-repo:v2.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push sammydev3564/my-repo:v2.0'
+        sh 'docker push sammydev3564/my-repo:v2.1'
     }
 } 
 
